bf701b05d4c3dd5d3f952df80acc2b53ee8cb708
Vinícius Alves <vinialves022@gmail.com>
2020-10-08 18:15:49 -0300

    Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './core/assets/styles/custom.scss'
+import './App.scss';
+import Navibar from './core/components/Navbar';
 
 const App = () => {
 
     return (
-        <h1>App</h1>
+        <Navibar />
     );
 }
 

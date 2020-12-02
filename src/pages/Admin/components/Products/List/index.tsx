0fa896b4844536f85b79e7be7bcd9c8cf01379b1
Avonts <vinialves022@gmail.com>
2020-12-02 11:33:06 -0300

    Integrate product listing with backend

--- src/pages/Admin/components/Products/List/index.tsx
+++ src/pages/Admin/components/Products/List/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import { useHistory} from 'react-router-dom';
+import Pagination from 'core/components/Pagination';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Card from '../Card';
 
 const List = () => {
 
     const history = useHistory();
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
+
+    console.log(productsResponse);
+
+    useEffect(() => {
+
+        const params = {
+            page: activePage,
+            linesPerPage: 4
+        }
+
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }, [activePage]);
+
 
     const handleCreate = () => {
         history.push('/admin/products/create');
@@ -17,9 +41,17 @@ const List = () => {
             </button>
 
             <div className="admin-list-container">
-                <Card />
-                <Card />
-                <Card />
+                {productsResponse?.content.map(product => (
+                    <Card product={product} key={product.id} />
+                ))}
+
+                {productsResponse && (
+                    <Pagination
+                        totalPages={productsResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />
+                )}
             </div>
         </div>
     )

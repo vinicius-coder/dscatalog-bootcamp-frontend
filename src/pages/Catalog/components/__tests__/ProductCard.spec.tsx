5e08f54c85677d0de2c2e04981102968f3bed001
Avonts <vinialves022@gmail.com>
2021-01-21 23:00:28 -0300

    Add ProductCard tests

---
+++ src/pages/Catalog/components/__tests__/ProductCard.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ProductCard from '../ProductCard';
+import { Product } from 'core/types/Product';
+
+test.only('Shound render ProductCard', () => {
+
+    const product = {
+        name: 'computador',
+        imgUrl: 'image.jpg',
+        price: 40
+    } as Product;
+
+    render(
+        <ProductCard product={product} />
+    );
+
+    expect(screen.getByText('computador')).toBeInTheDocument();
+    expect(screen.getByAltText('computador')).toBeInTheDocument();
+
+    expect(screen.getByText('R$')).toBeInTheDocument();
+    expect(screen.getByText('40.00')).toBeInTheDocument();
+
+});

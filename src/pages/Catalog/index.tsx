0dfa244dfd8a627b9fc77fcda2d6e3b601b3896c
Avonts <vinialves022@gmail.com>
2021-02-18 12:29:29 -0300

    Add product filters

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard';
 import { makeRequest } from 'core/utils/request';
 import { ProductsResponse } from 'core/types/Product';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import Pagination from 'core/components/Pagination';
+import ProductFilters, { FilterForm } from 'core/components/ProductFilters';
 import './styles.scss';
-
 
 const Catalog = () => {
 
@@ -14,11 +14,12 @@ const Catalog = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [activePage, setActivePage] = useState(0);
 
-    useEffect(() => {
-
+    const getProducts = useCallback((filter?: FilterForm) => {
         const params = {
             page: activePage,
-            linesPerPage: 12
+            linesPerPage: 12,
+            name: filter?.name,
+            categoryId: filter?.categoryId,
         }
 
         setIsLoading(true);
@@ -29,12 +30,19 @@ const Catalog = () => {
             })
     }, [activePage]);
 
+    useEffect(() => {
+        getProducts()
+    }, [getProducts]);
+
     return (
 
         <div className="catalog-container">
-            <h1 className="catalog-title">
-                Catálogo de produtos
-        </h1>
+            <div className="d-flex justify-content-between">
+                <h1 className="catalog-title">
+                    Catálogo de produtos
+                </h1>
+                <ProductFilters onSearch={filter => getProducts(filter)} />
+            </div>
             <div className="catalog-products">
                 {isLoading ? <ProductCardLoader /> : (
                     productsResponse?.content.map(product => (
